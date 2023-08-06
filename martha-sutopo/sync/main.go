02d7b1d3f9bd9afc8630aee75375845ec437c632
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-08-06 12:01:50 +0700

    add: wait group

--- martha-sutopo/sync/main.go
+++ martha-sutopo/sync/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
 func main() {
-	
+	wg.Add(2)
+	go CetakNama("Hi")
+	go CetakNama("Hallo")
+	wg.Wait()
+
+	fmt.Println("program selesai")
+}
+
+func CetakNama(s string) {
+	defer wg.Done()
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("%s ke %d\n", s, i)
+		<-time.After(time.Second)
+	}
 }
